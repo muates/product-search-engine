58f18ff01e17729027af8fd54cad2bcad2e6d36b
Murat Ates <mrtt.atss@gmail.com>
2024-02-11 18:56:01 +0300

    Product Entity added

---
+++ product-service/src/main/java/com/muates/productservice/model/entity/ProductEntity.java
@@ -0,0 +1,58 @@
+package com.muates.productservice.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false, length = 50)
+    private String brand;
+
+    @Column(length = 50)
+    private String model;
+
+    @Column(nullable = false)
+    private int stockQuantity;
+
+    @Column(nullable = false)
+    private int minStockQuantity;
+
+    @Column(length = 50)
+    private String category;
+
+    @Column(length = 255)
+    private String imageUrl;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date createdAt;
+}
