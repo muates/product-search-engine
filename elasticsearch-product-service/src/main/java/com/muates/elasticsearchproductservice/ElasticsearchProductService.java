985fff920b8435cc509d7b4660bd9444276e1f65
Murat Ates <mrtt.atss@gmail.com>
2024-02-15 21:23:58 +0300

    ...

---
+++ elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/ElasticsearchProductService.java
@@ -0,0 +1,11 @@
+package com.muates.elasticsearchproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchProductService {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchProductService.class, args);
+    }
+}
