28557e15d13273ceff90098aaf5d0cff1c403d8e
Murat Ates <mrtt.atss@gmail.com>
2024-02-17 21:57:25 +0300

    ...

--- product-service/src/main/java/com/muates/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/muates/productservice/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package com.muates.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.muates"})
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
