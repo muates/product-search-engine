d4f251b27c68238655ba79d57464ce9b4265bb72
Murat Ates <mrtt.atss@gmail.com>
2024-02-17 21:57:10 +0300

    ...

--- elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/ElasticProductServiceApplication.java
+++ elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/ElasticProductServiceApplication.java
@@ -2,10 +2,12 @@ package com.muates.elasticsearchproductservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.muates"})
 @SpringBootApplication
-public class ElasticsearchProductService {
+public class ElasticProductServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ElasticsearchProductService.class, args);
+        SpringApplication.run(ElasticProductServiceApplication.class, args);
     }
 }
--- elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/service/ElasticProductService.java
+++ elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/service/ElasticProductService.java
@@ -5,6 +5,7 @@ import com.muates.elasticsearchproductservice.model.ProductIndex;
 import java.util.List;
 import java.util.UUID;
 
-public interface ProductElasticService {
+public interface ElasticProductService {
     List<UUID> save(List<ProductIndex> documents);
+    List<ProductIndex> searchProducts(String query);
 }
