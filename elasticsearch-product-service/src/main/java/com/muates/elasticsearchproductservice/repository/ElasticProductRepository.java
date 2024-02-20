67445fdfd8b10fc94ad8d388fa3806d969324d39
Murat Ates <mrtt.atss@gmail.com>
2024-02-20 21:26:37 +0300

    product search updated

--- elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/repository/ElasticProductRepository.java
+++ elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/repository/ElasticProductRepository.java
@@ -1,28 +1,15 @@
 package com.muates.elasticsearchproductservice.repository;
 
 import com.muates.elasticsearchproductservice.model.ProductIndex;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ElasticProductRepository extends ElasticsearchRepository<ProductIndex, UUID> {
-
-    @Query("{" +
-            "\"bool\": {" +
-            "    \"should\": [" +
-            "        {" +
-            "            \"multi_match\": {" +
-            "                \"query\": \"?0\"," +
-            "                \"fields\": [\"name^1\", \"brand^1\", \"model\", \"category\"]" +
-            "            }" +
-            "        }" +
-            "    ]" +
-            "}" +
-            "}")
-    List<ProductIndex> searchProducts(@Param("query") String query);
+    Long countByName(String name);
+    Long countByCategory(String category);
+    Long countByBrand(String brand);
+    Long countByModel(String model);
 }
--- elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/service/impl/ElasticProductServiceImpl.java
+++ elasticsearch-product-service/src/main/java/com/muates/elasticsearchproductservice/service/impl/ElasticProductServiceImpl.java
@@ -3,8 +3,14 @@ package com.muates.elasticsearchproductservice.service.impl;
 import com.muates.elasticsearchproductservice.model.ProductIndex;
 import com.muates.elasticsearchproductservice.repository.ElasticProductRepository;
 import com.muates.elasticsearchproductservice.service.ElasticProductService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -18,9 +24,12 @@ public class ElasticProductServiceImpl implements ElasticProductService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticProductServiceImpl.class);
 
     private final ElasticProductRepository elasticProductRepository;
+    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
 
-    public ElasticProductServiceImpl(ElasticProductRepository elasticProductRepository) {
+    public ElasticProductServiceImpl(ElasticProductRepository elasticProductRepository,
+                                     ElasticsearchRestTemplate elasticsearchRestTemplate) {
         this.elasticProductRepository = elasticProductRepository;
+        this.elasticsearchRestTemplate = elasticsearchRestTemplate;
     }
 
     @Override
@@ -45,7 +54,61 @@ public class ElasticProductServiceImpl implements ElasticProductService {
             searchResults = (product != null) ? Collections.singletonList(product) : Collections.emptyList();
             logProductSearchResult(query, searchResults);
         } else {
-            searchResults = elasticProductRepository.searchProducts(query);
+            String[] queryParts = query.split("\\s+");
+
+            String name = null;
+            String brand = null;
+            String model = null;
+            String category = null;
+            int minMatchFields = 0;
+
+            for (String part : queryParts) {
+                if (elasticProductRepository.countByCategory(part) > 0) {
+                    category = (category != null) ? category + " " + part: part;
+                    continue;
+                }
+                if (elasticProductRepository.countByBrand(part) > 0) {
+                    brand = (brand != null) ? brand + " " + part : part;
+                    continue;
+                }
+
+                if (elasticProductRepository.countByModel(part) > 0) {
+                    model = (model != null) ? model + " " + part : part;
+                    continue;
+                }
+                if (elasticProductRepository.countByName(part) > 0) {
+                    name = (name != null) ? (name + " " + part) : part;
+                }
+            }
+
+            BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+
+            if (name != null) {
+                boolQuery.should(QueryBuilders.matchQuery("name", name).boost(1));
+                minMatchFields++;
+            }
+
+            if (brand != null) {
+                boolQuery.should(QueryBuilders.matchQuery("brand", brand).boost(2));
+                minMatchFields++;
+            }
+
+            if (model != null) {
+                boolQuery.should(QueryBuilders.matchQuery("model", model).boost(2));
+                minMatchFields++;
+            }
+
+            if (category != null) {
+                boolQuery.should(QueryBuilders.matchQuery("category", category).boost(1));
+                minMatchFields++;
+            }
+
+            boolQuery.minimumShouldMatch(minMatchFields);
+
+            SearchHits<ProductIndex> searchHits = elasticsearchRestTemplate.search(
+                    new NativeSearchQueryBuilder().withQuery(boolQuery).build(), ProductIndex.class);
+
+            searchResults = searchHits.stream().map(SearchHit::getContent).collect(Collectors.toList());
             logProductSearchResult(query, searchResults);
         }
 
